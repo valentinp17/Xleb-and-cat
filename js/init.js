b32c7fa68b0b3e54e54d3f8244127935c0ee8727
Valentin <pvv377@gmail.com>
2019-05-04 01:09:59 +0300

    Init commit

---
+++ js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+function play(){
+  var audio = document.getElementById("audio");
+  audio.play();
+}
